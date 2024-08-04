8a5d05181fbd44af2ecd39f41e3d4f40459e448e
Raúl Jiménez Galán <truenodeveloper@gmail.com>
2024-08-04 03:50:30 +0200

    First

---
+++ players.database.js
@@ -0,0 +1,32 @@
+module.exports = {
+    players: [
+        {
+            discordId: "281716191406325762",
+            playerName: "ehl_trueno"
+        },
+        {
+            discordId: "568099830916710400",
+            playerName: "Cjr37"
+        },
+        {
+            discordId: "888778679793098842",
+            playerName: "elejr7"
+        },
+        {
+            discordId: "1016463966186262530",
+            playerName: "DonPipas"
+        },
+        {
+            discordId: "496019064725176341",
+            playerName: "FloAvN90"
+        },
+        {
+            discordId: "778534123387224064",
+            playerName: "Samuel_blak"
+        },
+        {
+            discordId: "253102443506827269",
+            playerName: "SitoX96"
+        }
+    ]
+}
